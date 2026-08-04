6219e16ca95beac3f3615bc9fcd91a45f2248a3b
Ethan Williams <ethan.williams@mobilefirst.app>
2026-08-04 18:56:38 -0400

    Initial commit

--- src/test/java/com/harbormaster/controller/TestSpringRestController.java
+++ src/test/java/com/harbormaster/controller/TestSpringRestController.java
@@ -21,7 +21,7 @@
  * Contributors :
  *       Turnstone Biologics - General Release
  */
-package com.harbormaster.controller;
+		package com.harbormaster.controller;
 
 import java.io.*;
 import java.util.logging.*;
@@ -30,8 +30,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import org.springframework.context.ApplicationContext;
 
-/** 
+/**
  * Test class for all Rest Controller delegation functions
  *
  * @author Harbormaster Dev Team
@@ -39,49 +40,54 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class TestSpringRestController
 {
+	public TestSpringRestController( ApplicationContext ctx ) {
+		this.ctx = ctx;
+	}
 	@RequestMapping("/")
-    ModelAndView home() 
+	ModelAndView home()
 	{
-        return new ModelAndView("startTest", "message", "sample message" );
-    }
+		return new ModelAndView("startTest", "message", "sample message" );
+	}
 
 	/**
-     * Handles calling the underlying JUnit testing facility
-     */
+	 * Handles calling the underlying JUnit testing facility
+	 */
 	@RequestMapping("/test")
-    public String test()
-    {
+	public String test()
+	{
 		ByteArrayOutputStream os 	= new java.io.ByteArrayOutputStream();
 		StreamHandler handler 		= new StreamHandler( os, new LogTestFormatter() );
-		
-		com.harbormaster.test.BaseTest.runTheTest(handler);
-		
+
+		com.harbormaster.test.BaseTest.runTheTest(ctx, handler);
+
 		return( os.toString() );
-    }
-	
-	private StreamHandler handler = null;
-	
-	class LogTestFormatter extends Formatter 
-	{
-	    public String format(LogRecord record) 
-	    {
-	        StringBuilder builder = new StringBuilder(1000);
-	        
-	        if ( record.getLevel() == Level.WARNING )
-	        	builder.append( "<span><style='color:red'>" );
-	        else if ( record.getLevel() == Level.SEVERE )
-	        	builder.append( "<span><style='color:red;font-weight:bolder'>" );
-	        else
-	        	builder.append( "<span><style='color:black'>" );
-	        
-	        builder.append(formatMessage(record));
-
-	        builder.append( "</style></span>" );
-
-	        builder.append("<br>");
-	        return builder.toString();
-	    }
 	}
+
+	private StreamHandler handler = null;
+
+	class LogTestFormatter extends Formatter
+	{
+		public String format(LogRecord record)
+		{
+			StringBuilder builder = new StringBuilder(1000);
+
+			if ( record.getLevel() == Level.WARNING )
+				builder.append( "<span><style='color:red'>" );
+			else if ( record.getLevel() == Level.SEVERE )
+				builder.append( "<span><style='color:red;font-weight:bolder'>" );
+			else
+				builder.append( "<span><style='color:black'>" );
+
+			builder.append(formatMessage(record));
+
+			builder.append( "</style></span>" );
+
+			builder.append("<br>");
+			return builder.toString();
+		}
+	}
+
+	private ApplicatonContext ctx;
 }
 
 

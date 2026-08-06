abe35e294ef8e572d8f6aa95f145743414f4ede7
Ethan Williams <ethan.williams@mobilefirst.app>
2026-08-05 23:42:17 -0400

    Initial commit

--- src/main/java/com/harbormaster/config/FormLoginSecurityConfiguration.java
+++ src/main/java/com/harbormaster/config/FormLoginSecurityConfiguration.java
@@ -2,7 +2,7 @@ package com.harbormaster.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
